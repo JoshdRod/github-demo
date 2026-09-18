b127baa299b9444949d5c56bd9227d393d2a7e8e
rai-dan <daniyal.rai@arm.com>
2026-09-18 12:15:47 +0100

    Fixed deposit

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 def deposit(balance):
     amount_to_deposit = int(input("How much would you like to deposit? "))
-    balance = balance - amount_to_deposit
+    balance = balance + amount_to_deposit
     return balance
 
 def withdraw(balance):
     amount = int(input("How much would you like to withdraw"))
     return balance - amount
-    pass # TODO: This function should ask the user how much they want to withdraw, and subtract that from their balance. Don't worry about invalid inputs - assume the atm user is a smart guy. Return the balance.
-
+    
 def display(balance):
     print(balance)
 
